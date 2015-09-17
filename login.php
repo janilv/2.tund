82430185c4ba8bd0cd15bc39bb21d7d3edcf1920
Jan-Erik Ilves <janilv@greeny.cs.tlu.ee>
2015-09-17 13:59:08 +0300

    Create user age + MVP idee

--- login.php
+++ login.php
@@ -55,6 +55,12 @@
 	<input name="pass" type="password" placeholder="Parool ">*<?php echo $password_error ?><br><br>
 	<input name="name" type="first_name" placeholder="Eesnimi">*<br><br>
 	<input name="name" type="last_name" placeholder="Perekonnanimi">*<br><br>
-
+	<input name="date" type="age" placeholder="Vanus">*<br><br>
+	<input type="submit" value="Registeeri">
+	</form>
+	MVP idee
+	Idee seisneb selles, et inimesed, kes käivad jõusaalis, saaks mugavalt oma järgida oma toitumist.
+	 Koostaks kalkulaatori(vanus, rasvaprotsent, kui palju liigutakse nädalas) ja vastavalt oma soovidele(kaalu kaotus/massi suurendamine) näeks inimene, kuidas toiteväärtust muuta ja kuidas õigesti süüa.
+	 Et asja natuke omamoodi luua, siis võiks olla lisa võimalus, et koostab päeva ja hiljem nädalaplaani ja lisab sellele kõrvale retseptid, et poest lihtsam asju osta oleks ja saaks täpselt toitumisnõuded ära täita.
 </body>
 </html>
